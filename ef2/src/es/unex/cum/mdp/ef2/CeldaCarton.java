053d012f634c33072f89187f9e4c338694522714
fredy <fredy@LAPTOP-975D6GEF>
2023-11-06 14:28:26 +0100

    Clase Celda Cartón

---
+++ ef2/src/es/unex/cum/mdp/ef2/CeldaCarton.java
@@ -0,0 +1,11 @@
+package es.unex.cum.mdp.ef2;
+
+public class CeldaCarton extends Celda {
+	private int fila;
+	private int columna;
+	private boolean especial;
+	private String RutaImagen;
+	private String RutaReverso;
+	
+
+}
