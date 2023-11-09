3f010c3352855988c08fcd364f6e36e209b1a035
fredy <fredy@LAPTOP-975D6GEF>
2023-11-09 15:24:00 +0100

    Otras clases Cartón

---
+++ ef2/src/es/unex/cum/mdp/ef2/Carton75V.java
@@ -0,0 +1,113 @@
+package es.unex.cum.mdp.ef2;
+
+public class Carton75V implements ICarton{
+
+	@Override
+	public EstadoCarton getEstado() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getAciertos() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setId(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Usuario getUser() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setUser(Usuario user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean comprobarLinea() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean comprobarLinea(int fila) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean recibirNumero(int numero) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean comprobarBingo() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean comprobarEspeciales() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean repartir() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addNumero(CeldaCarton c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addEspecial(int num) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public float getPrecio() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setPremio(float valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public float getPremio() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getNumeros() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
